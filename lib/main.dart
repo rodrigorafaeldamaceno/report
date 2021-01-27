6f91f2170a5275e33ac256252ec5b1ac7b9b6bc1
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2021-01-27 19:40:11 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:report/pages/report_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Report App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ReportPage(),
+    );
+  }
+}
